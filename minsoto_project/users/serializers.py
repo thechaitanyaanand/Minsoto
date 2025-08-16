09b701b375fefc9566f43356c6deb20b13a65fa4
Chaitanya Anand <thechaitanyaanand@gmail.com>
2025-08-16 22:14:55 +0530

    Revert "login and register ui finalized but actual login and register is buggy and currently not working"
    This reverts commit 80cd65a91a905bda2e00bd6bdde0fdb41cb72ee4.

--- minsoto_project/users/serializers.py
+++ minsoto_project/users/serializers.py
@@ -1,98 +1,22 @@
 from rest_framework import serializers
-from dj_rest_auth.registration.serializers import RegisterSerializer as DefaultRegisterSerializer
-from .models import (
-    CustomUser,
-    Circle,
-    Post,
-    Interest,
-    Connection,
-    Friendship,
-    Habit,
-    Streak
-)
+from .models import CustomUser
 
-# --- Authentication Serializers ---
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
 
-class RegisterSerializer(DefaultRegisterSerializer):
-    # We are extending the default serializer to ensure it uses our custom fields
-    # if we add them later, like a username.
-    username = serializers.CharField(required=True, max_length=150)
-
-    def custom_signup(self, request, user):
-        user.username = self.validated_data.get('username', '')
-        user.save(update_fields=['username'])
-
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the CustomUser model, used for retrieving user details.
-    """
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture_url', 'banner_image_url')
+        fields = ('username', 'password', 'email')
 
-# --- Core Feature Serializers ---
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
 
-class InterestSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Interest
-        fields = '__all__'
-
-class CircleSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField()
-    member_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Circle
-        fields = ('id', 'name', 'description', 'circle_type', 'owner', 'member_count', 'created_at')
-
-    def get_member_count(self, obj):
-        return obj.members.count()
-
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
-    circle = serializers.StringRelatedField()
-
-    class Meta:
-        model = Post
-        fields = ('id', 'author', 'circle', 'content', 'created_at', 'is_pinned')
-
-# --- Connection and Friendship Serializers ---
-
-class FriendshipSerializer(serializers.ModelSerializer):
-    requester = serializers.StringRelatedField(read_only=True)
-    receiver_username = serializers.CharField(source='receiver.username', read_only=True)
-
-    class Meta:
-        model = Friendship
-        fields = ('id', 'requester', 'receiver', 'receiver_username', 'status', 'created_at')
-        extra_kwargs = {
-            'receiver': {'write_only': True},
-        }
-
-class ConnectionSerializer(serializers.ModelSerializer):
-    requester = serializers.StringRelatedField(read_only=True)
-    receiver_username = serializers.CharField(source='receiver.username', read_only=True)
-    interest_name = serializers.CharField(source='interest.name', read_only=True)
-
-    class Meta:
-        model = Connection
-        fields = ('id', 'requester', 'receiver', 'receiver_username', 'interest', 'interest_name', 'status', 'created_at')
-        extra_kwargs = {
-            'receiver': {'write_only': True},
-            'interest': {'write_only': True},
-        }
-
-# --- Habit and Streak Serializers ---
-
-class StreakSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Streak
-        fields = ('current_streak', 'last_checkin_date')
-
-class HabitSerializer(serializers.ModelSerializer):
-    streak = StreakSerializer(read_only=True)
-    user = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Habit
-        fields = ('id', 'user', 'circle', 'name', 'created_at', 'streak')
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture_url')
